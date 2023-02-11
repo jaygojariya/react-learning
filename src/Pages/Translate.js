87f3146054d3da61b2d5af9f4ac6784e08fdf3a6
Jay Gojariya <jgojariya@sigmasolve.net>
2023-02-11 17:33:11 +0530

    react-i18next - multi language app

--- src/Pages/Translate.js
+++ src/Pages/Translate.js
@@ -1,7 +1,43 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import i18next from "i18next";
 
 export default function Translate() {
+
+    const { t } = useTranslation();
+    const [selectedLanguage, setSelectedLanguage] = React.useState('en');
+
+    const handleLanguageChange = (language) => {
+        setSelectedLanguage(language);
+        i18next.changeLanguage(language);
+    };
+
     return (
-        <h1>Translate</h1>
+        <div>
+            <div>
+                <input
+                    type="radio"
+                    id="english"
+                    name="language"
+                    value="en"
+                    checked={selectedLanguage === 'en'}
+                    onChange={() => handleLanguageChange('en')}
+                />
+                <label htmlFor="english">English</label>
+            </div>
+            <div>
+                <input
+                    type="radio"
+                    id="hindi"
+                    name="language"
+                    value="hi"
+                    checked={selectedLanguage === 'hi'}
+                    onChange={() => handleLanguageChange('hi')}
+                />
+                <label htmlFor="hindi">Hindi</label>
+            </div>
+            <p><b>Static Sentence : </b>{t('This is static content')}</p>
+            <p><b>Dynamic Parameters : </b>{t('Selected : {{ selectedLanguage }}', { selectedLanguage: selectedLanguage })}</p>
+        </div>
     );
 }
